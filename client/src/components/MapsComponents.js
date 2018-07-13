8e18cb85ec74fccadf5eb42e05c49ca15bcf891f
Joseph Kim <joekimga@gmail.com>
2018-07-12 21:43:44 -0400

    googlemaps info migrates to joeAndJames branch from Milestone1 branch

---
+++ client/src/components/MapsComponents.js
@@ -0,0 +1,55 @@
+import React from "react";
+// import ApiKey from "../../Apikey";
+import { compose, withProps } from "recompose"
+import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps"
+
+const MapsComponents = compose(
+  withProps({
+    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
+    loadingElement: <div style={{ height: `100%` }} />,
+    containerElement: <div style={{ height: `400px` }} />,
+    mapElement: <div style={{ height: `100%` }} />,
+  }),
+  withScriptjs,
+  withGoogleMap
+)((props) =>
+  <GoogleMap
+    defaultZoom={16}
+    defaultCenter={{lat: props.centerLat, lng: props.centerLong }}
+    // defaultCenter={{lat: 33.7764, lng: -84.3893 }}
+  >
+    {props.isMarkerShown && props.children}
+  </GoogleMap>
+)
+
+// export class MapsComponents extends React.PureComponent {
+//   state = {
+//     isMarkerShown: false,
+//   }
+
+//   componentDidMount() {
+//     this.delayedShowMarker()
+//   }
+
+//   delayedShowMarker = () => {
+//     setTimeout(() => {
+//       this.setState({ isMarkerShown: true })
+//     }, 3000)
+//   }
+
+//   handleMarkerClick = () => {
+//     this.setState({ isMarkerShown: false })
+//     this.delayedShowMarker()
+//   }
+
+//   render() {
+//     return (
+//       <MyFancyComponent
+//         isMarkerShown={this.state.isMarkerShown}
+//         onMarkerClick={this.handleMarkerClick}
+//       />
+//     )
+//   }
+// }
+
+export default MapsComponents;
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,4 +1,10 @@
 import axios from "axios";
+const milestone1ZipCodeURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=";
+const zipcodeAPIkey = "&key=AIzaSyDOIrdjOme0yAiuQS8QlE9JTucD7rG81X8";
+
+const getPlaceHours = "https://maps.googleapis.com/maps/api/place/details/json?placeid=";
+
+
 
 export default {
  //Parking deck information
@@ -17,6 +23,18 @@ export default {
   // Saves a parking deck to the database
   saveParking: function(parkingData) {
     return axios.post("/api/parking", parkingData);
+  },//ZIP CODE FOR RESTAURANTS AND BARS 
+  getZipPlaces: function(zipcode) {
+    return axios.get(milestone1ZipCodeURL+"food+in+Atlanta+"+zipcode+zipcodeAPIkey);
+  // return axios.get("https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat+","+lng+"&radius=2500&type=restaurant"+zipcodeAPIkey)
+  // return axios.get ("https://maps.googleapis.com/maps/api/geocode/json?address="+zipcode+zipcodeAPIkey);
+  },
+  //PLACE HOURS FOR RESTAURANTS AND BARS
+  getPlaceHours: function(placeid) {
+    return axios.get(getPlaceHours+placeid+zipcodeAPIkey+"&fields=name,opening_hours");
+  },
+  getCordinates: function(zipcode) {
+    return axios.get("/mygeocoder/" +zipcode)
   }
 };
 
