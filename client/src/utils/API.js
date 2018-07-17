72bedc0c58676e2d8e10391368dada4b08fb47f7
Joseph Kim <joekimga@gmail.com>
2018-07-17 19:38:34 -0400

    hiding keys

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,6 +1,9 @@
 import axios from "axios";
+import configkeys from "../config.js"
+
 const milestone1ZipCodeURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=";
-const zipcodeAPIkey = "&key=AIzaSyDOIrdjOme0yAiuQS8QlE9JTucD7rG81X8";
+// Import from .env file
+// const zipcodeAPIkey = "&key=AIzaSyDOIrdjOme0yAiuQS8QlE9JTucD7rG81X8";
 
 const getPlaceHours = "https://maps.googleapis.com/maps/api/place/details/json?placeid=";
 
@@ -25,13 +28,13 @@ export default {
     return axios.post("/api/parking", parkingData);
   },//ZIP CODE FOR RESTAURANTS AND BARS 
   getZipPlaces: function(zipcode) {
-    return axios.get(milestone1ZipCodeURL+"food+in+Atlanta+"+zipcode+zipcodeAPIkey);
+    return axios.get(milestone1ZipCodeURL+"food+in+Atlanta+"+zipcode+configkeys.zipcodeAPIkey);
   // return axios.get("https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat+","+lng+"&radius=2500&type=restaurant"+zipcodeAPIkey)
   // return axios.get ("https://maps.googleapis.com/maps/api/geocode/json?address="+zipcode+zipcodeAPIkey);
   },
   //PLACE HOURS FOR RESTAURANTS AND BARS
   getPlaceHours: function(placeid) {
-    return axios.get(getPlaceHours+placeid+zipcodeAPIkey+"&fields=name,opening_hours");
+    return axios.get(getPlaceHours+placeid+configkeys.zipcodeAPIkey+"&fields=name,opening_hours");
   },
   getCordinates: function(zipcode) {
     return axios.get("/mygeocoder/" +zipcode)
