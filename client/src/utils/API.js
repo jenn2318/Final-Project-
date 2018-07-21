057e12066f83a2903fd9067c92ac2bf94582756e
Sarah Shelden <sarah.shelden113@gmail.com>
2018-07-21 11:22:34 -0400

    places api

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,13 +1,21 @@
 import axios from "axios";
 import configkeys from "../config.js"
 
-const milestone1ZipCodeURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=";
+const config = {
+    headers : {
+        'Access-Control-Allow-Origin': 'http://localhost:3000'
+    }
+};
+
+// const milestone1ZipCodeURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/textsearch/json?query=";
 // Import from .env file
 
 
 const getPlaceHours = "https://maps.googleapis.com/maps/api/place/details/json?placeid=";
 
 const geolocate = "https://www.googleapis.com/geolocation/v1/geolocate?key=";
+
+const placesByLatAndLong = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=33.7753097,-84.3909888&radius=1500&type=restaurant&key=";
 
 export default {
  //Parking deck information
@@ -26,9 +34,12 @@ export default {
   // Saves a parking deck to the database
   saveParking: function(parkingData) {
     return axios.post("/api/parking", parkingData);
+
+
   },//ZIP CODE FOR RESTAURANTS AND BARS 
-  getZipPlaces: function(zipcode) {
-    return axios.get(milestone1ZipCodeURL+"food+in+Atlanta+"+zipcode+configkeys.zipcodeAPIkey);
+  getZipPlaces: function() {
+    return axios.get(placesByLatAndLong+configkeys.zipcodeAPIkey,config)
+    // return axios.get(milestone1ZipCodeURL+"food+in+Atlanta+"+zipcode+configkeys.zipcodeAPIkey);
   // return axios.get("https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?location="+lat+","+lng+"&radius=2500&type=restaurant"+zipcodeAPIkey)
   // return axios.get ("https://maps.googleapis.com/maps/api/geocode/json?address="+zipcode+zipcodeAPIkey);
   },
